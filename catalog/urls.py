f655c22d7b5ef6aa75f9bad3d4441f2f4a4045cd
MichaelMigachev <migachev21@gmail.com>
2023-12-06 21:33:06 +0300

    выполнил ДЗ 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from catalog.views import index
+from catalog.views import index, categories
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', include('catalog.urls', namespace='catalog'))
-    path('', index)
+    path('', index, name='index'),
+    path('categories/', categories, name='categories'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all()[:3],
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+def categories(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'каталог'
+    }
+    return render(request, 'catalog/categories.html', context)
